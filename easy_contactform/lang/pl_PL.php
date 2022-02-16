4ce37a8f860849463dffb47fd043acb29a3a4c95
multicolor-rgb <83913997+multicolor-rgb@users.noreply.github.com>
2022-02-16 10:56:37 +0100

    polish lang added

---
+++ easy_contactform/lang/pl_PL.php
@@ -0,0 +1,42 @@
+<?php 
+
+	$i18n = array(
+'EASYNAMEONMENU'=>'Easy_contactForm ustawienia',
+		'SENDER' => 'Nadawca:',
+    'DELIVER' =>'Na jaką pocztę dostarczyć wiadomość? (przykład@poczta.com, przykład2@poczta.com)',
+    'SUBJECTMESS' =>'Temat wiadomości ze strony internetowej',
+    'TEXTSUCCESS' =>'Tekst, gdy wiadomość została wysłana pomyślnie',
+    'TEXTNOSUCCESS' => 'Tekst, gdy wiadomość nie została wysłana pomyślnie',
+    'LABELSMAIL' => 'Etykiety w wiadomości e-mail',
+
+    'FROMLABEL' =>'Etykieta „Od”',
+    'EMAILLABEL' =>'Etykieta „e-mail”',
+    'PHONELABEL' =>'Etykieta „Telefon”',
+    'CONTENTLABEL' =>'Etykieta „Treść”',
+    'SAVESETTINGS' =>'Zapisz ustawienia',
+
+    'SUPPORT' =>'Jeśli chcesz wesprzeć moją pracę przez paypal :) Dzięki!',
+    'CREATED' =>'Stworzone przez',
+  
+
+    //options cke
+    'REQ' =>'Wymagany?',
+    'CKNAME' =>'Etykieta na imię i nazwisko',
+    'CKPHONE' =>'Etykieta na telefon',
+    'CKMAIL' =>'Etykieta na e-mail',
+    'CKMESS' =>'Etykieta wiadomości',
+    'CKPRIVACY' => 'Treść polityki prywatności',
+    'CKREQ' =>'Tekst w polu „wymagane”',
+    'CKSUBMIT' =>'Tekst przycisku „prześlij”',
+'CKCOLOR' =>'Kolor przycisku „Wyślij”',
+'CKHOVERCOLOR' =>'Kolor przycisku „Wyślij” po najechaniu',
+
+//buttons
+
+'ADDFORM' =>'Dodaj formularz',
+'REMOVEFORM' =>'Usuń formularz',
+'CLOSE' =>'Zamknij',
+
+    );
+    
+    ?>
